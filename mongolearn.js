59a515e23b32a90f8adefa81f8cd284fa3833aff
Sean Pe <sean.m.s.pe@gmail.com>
2020-02-22 14:28:13 +0800

    functional user controller, schema, and add to database

--- mongolearn.js
+++ mongolearn.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose')
-const roundRoom = require('./controllers/roundRoom.controller.js')
+const userController = require('./controllers/userController.js')
+const connectionString = 'mongodb+srv://seanUserAtlas:WjPqgpp6GRkQOuhy@cluster0-hlmd5.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
-var url = "mongodb+srv://seanUserAtlas:WjPqgpp6GRkQOuhy@debateroundroom-hlmd5.gcp.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true });
+
+;(async () => {
+  const connector = mongoose.connect(connectionString)
+  const username = "hello"
+
+  let user = await connector.then(async () => {
+    return userController.findUser(username)
+  })
+
+  if (!user) {
+    user = await userController.createUser(username)
+  }
+
+  console.log(user)
+  process.exit(0)
+})()
